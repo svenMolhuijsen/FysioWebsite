bb66031042ce7188fde04c4184623429a8ac27c4
Sven molhuijsen <s.molhuijsen@gmail.com>
2015-09-18 14:08:09 +0200

    heb delen als includes aangemaakt

--- index.php
+++ index.php
@@ -1,76 +1,8 @@
-<!DOCTYPE html>
-<html lang="nl">
-
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title></title>
-    <!-- Bootstrap -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-    <link rel="stylesheet" href="content/css/style.css" />
-    <link href="content/css/roboto.min.css" rel="stylesheet">
-    <link href="content/css/material.min.css" rel="stylesheet">
-    <link href="content/css/ripples.min.css" rel="stylesheet">
-</head>
-
-<body>
-
-    <div class="navbar-wrapper">
-        <div class="container-fluid">
-            <nav class="navbar navbar-fixed-top">
-                <div class="container">
-                    <div class="navbar-header">
-                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                            <span class="sr-only">Toggle navigation</span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                        </button>
-                        <a class="navbar-brand" href="#">Logo</a>
-
-                    </div>
-                    <div id="navbar" class="navbar-collapse collapse">
-                        <ul class="nav navbar-nav">
-                            <li class="active"><a href="#" class="">Dashboard</a></li>
-
-                            <li class=" dropdown"><a href="#" class="dropdown-toggle " data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">App inhoud <span class="caret"></span></a>
-                                <ul class="dropdown-menu">
-                                    <li><a href="#">handleidingen</a></li>
-                                </ul>
-                            </li>
-                            <li class=" dropdown"><a href="#" class="dropdown-toggle " data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Gebruikers <span class="caret"></span></a>
-                                <ul class="dropdown-menu">
-                                    <li><a href="#">Clubs</a></li>
-                                    <li><a href="#">Clienten</a></li>
-                                </ul>
-                            </li>
-                            <li class=" dropdown"><a href="#" class="dropdown-toggle active" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Chat<span class="caret"></span></a>
-                                <ul class="dropdown-menu">
-                                    <li><a href="#">Nieuw</a></li>
-                                    <li><a href="#">Gelezen</a></li>
-                                    <li><a href="#">Archief</a></li>
-
-                                </ul>
-                            </li>
-                        </ul>
-                        <ul class="nav navbar-nav pull-right">
-                            <li class=" dropdown"><a href="#" class="dropdown-toggle active" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Ingelogd  <span class="caret"></span></a>
-                                <ul class="dropdown-menu">
-                                    <li><a href="#">Mijn Profiel</a></li>
-                                </ul>
-                            </li>
-                            <li class=""><a href="#">Uitloggen</a></li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-        </div>
-    </div>
+<?php include_once"content/php-include/header.php"; ?>
 
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-</body>
+    </body>
 
-</html>
+    </html>

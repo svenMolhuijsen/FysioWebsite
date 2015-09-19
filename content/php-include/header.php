d99f33d591cc6a265629b6af392466d74f3641c4
Sven molhuijsen <s.molhuijsen@gmail.com>
2015-09-19 11:13:13 +0200

    logo geupdate

--- content/php-include/header.php
+++ content/php-include/header.php
@@ -22,12 +22,12 @@
                 <div class="container">
                     <div class="navbar-header">
                         <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                            <span class="sr-only">Toggle navigation</span>
+                            <span class="sr-only">Menu</span>
                             <span class="icon-bar"></span>
                             <span class="icon-bar"></span>
                             <span class="icon-bar"></span>
                         </button>
-                        <a class="navbar-brand" href="#">Logo</a>
+                        <a class="navbar-brand" href="#">Fysiofitcare Potal</a>
 
                     </div>
                     <div id="navbar" class="navbar-collapse collapse">

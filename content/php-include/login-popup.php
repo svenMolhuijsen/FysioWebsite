e8a159ab7e7b97a2326b9facb082ae3a4dcc5c47
Sven molhuijsen <s.molhuijsen@gmail.com>
2015-09-25 10:44:56 +0200

    Heb registreren uit inlogscherm gesloopt

--- content/php-include/login-popup.php
+++ content/php-include/login-popup.php
@@ -32,7 +32,6 @@
                         <br>
                         <button type="submit" class="btn btn-primary">Log in</button>
                         <button id="login_lost_btn" type="button" class="btn btn-default">Wachtwoord vergeten?</button>
-                        <button id="login_register_btn" type="button" class="btn btn-default">Registreren</button>
                     </div>
 
 
@@ -52,7 +51,6 @@
                         <br>
                         <button type="submit" class="btn btn-primary ">Verstuur</button>
                         <button id="lost_login_btn" type="button" class="btn btn-default">Inloggen</button>
-                        <button id="lost_register_btn" type="button" class="btn btn-default">Registreren</button>
                     </div>
 
 
@@ -60,26 +58,7 @@
                 </form>
                 <!-- End | Lost Password Form -->
 
-                <!-- Begin | Register Form -->
-                <form id="register-form" style="display:none;">
-                    <div class="modal-body">
-                        <div id="div-register-msg">
-                            <div id="icon-register-msg" class="glyphicon glyphicon-chevron-right"></div>
-                            <span id="text-register-msg">Registreer een gebruiker</span>
-                        </div>
-                        <input id="register_username" class="form-control" type="text" placeholder="Username (type ERROR for error effect)" required>
-                        <input id="register_email" class="form-control" type="text" placeholder="E-Mail" required>
-                        <input id="register_password" class="form-control" type="password" placeholder="Wachtwoord" required>
-                        <button type="submit" class="btn btn-primary">Registreer</button>
-                        <button id="register_login_btn" type="button" class="btn btn-default">Inloggen</button>
-                        <button id="register_lost_btn" type="button" class="btn btn-default">Wachtwoord vergeten?</button>
-                    </div>
 
-
-
-            </div>
-            </form>
-            <!-- End | Register Form -->
 
         </div>
         <!-- End # DIV Form -->

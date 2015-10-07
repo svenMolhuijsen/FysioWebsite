31088244e8bb28129b06cd998b939aab58c8bff1
Sven molhuijsen <s.molhuijsen@gmail.com>
2015-10-07 14:26:44 +0200

    .

--- content/js/script.js
+++ content/js/script.js
@@ -48,8 +48,5 @@ $(document).ready(function () {
 
     $('[data-toggle="tooltip"]').tooltip();
 
-    $('button').click(function (e) {
-        e.preventDefault();
-        alert("This is a demo.\n :-)");
-    });
+
 });

2d0700f9f4612de1bdcaedf2ef418bed2474dc14
Bart Kessels <bartk2010@hotmail.com>
2015-10-12 10:32:49 +0200

    Opschonen

--- index.php
+++
@@ -1,6 +0,0 @@
-<?php require_once("content/php-include/header.php"); ?>
-<?php include_once "content/php-include/login-popup.php" ?>
-<div class="navbar-wrapper">
-    TODO: DASHBOARD
-</div>
-<?php require_once("content/php-include/footer.php"); ?>
